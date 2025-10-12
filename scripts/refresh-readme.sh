dc8f9346cb17c75fa8f14f3ee86e53f4b21a4c2b
Tomas Carnecky <tomc@caurea.org>
2025-10-12 21:21:52 +0200

    .

--- scripts/refresh-readme.sh
+++ scripts/refresh-readme.sh
@@ -11,9 +11,9 @@ pnpm_version=$(nix develop -c -- sh -c 'pnpm --version')
 biome_version=$(nix develop -c -- sh -c 'biome --version' | sed 's/Version: //')
 
 # Update the README.md
-sed -i '' "s/^> Last updated on: .*/> Last updated on: $current_date/" README.md
+sed -i "s/^> Last updated on: .*/> Last updated on: $current_date/" README.md
 
 # Update the versions table
-sed -i '' "s/| \[Node.js\](.*) | .* |/| [Node.js](https:\/\/nodejs.org\/) | $node_version | \`nodejs\` |/" README.md
-sed -i '' "s/| \[PNPm\](.*) | .* |/| [PNPm](https:\/\/pnpm.io\/) | $pnpm_version | \`pnpm\` |/" README.md
-sed -i '' "s/| \[Biome\](.*) | .* |/| [Biome](https:\/\/biomejs.dev\/) | $biome_version | \`biome\` |/" README.md
+sed -i "s/| \[Node.js\](.*) | .* |/| [Node.js](https:\/\/nodejs.org\/) | $node_version | \`nodejs\` |/" README.md
+sed -i "s/| \[PNPm\](.*) | .* |/| [PNPm](https:\/\/pnpm.io\/) | $pnpm_version | \`pnpm\` |/" README.md
+sed -i "s/| \[Biome\](.*) | .* |/| [Biome](https:\/\/biomejs.dev\/) | $biome_version | \`biome\` |/" README.md
